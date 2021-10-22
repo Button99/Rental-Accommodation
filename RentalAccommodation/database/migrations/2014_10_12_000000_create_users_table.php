ec602f0a5b69c770ea2e396e5abc247781c65108
Button99 <christoskoubis@yahoo.com>
2021-10-22 10:18:54 +0300

    Users table

--- RentalAccommodation/database/migrations/2014_10_12_000000_create_users_table.php
+++ RentalAccommodation/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('date_of_birth');
             $table->string('password');
+            $table->string('email')->unique();
+            $table->enum('gender', ['Male', 'Female', 'Non Binary']);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
