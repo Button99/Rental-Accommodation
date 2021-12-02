3124bbf05321b2553cbf1b712794cc3e01824e21
Button99 <christoskoubis@yahoo.com>
2021-12-02 20:06:13 +0200

    Footer done

--- RentalAccommodation/resources/js/components/Index.vue
+++ RentalAccommodation/resources/js/components/Index.vue
@@ -7,70 +7,62 @@
         <p>
             Here you can find every accommodation for you and your family so you can stay !
         </p>
-        <ul class="container">
+        <div class="container mx-auto mt-4">
             <div class="row">
-                <div class="col-md-3">
-                    <li class="list-group-item-action">
-                        <div class="card" style="width: 18em;">
-                            <div class="bg-image hover-overlay ripple">
-                                <img class="card-img-top img-fluid rounded" style=" height: 30vh;" :src=" getPhoto() +'pexels-alex-azabache-3879160.jpg'" />
-                            </div>
-                            <div class="card-body">
-                                <div class="card-title">Paris</div>
+                <div class="col-md-5">
+                    <div class="card" style="width: 18em;">
+                        <div class="bg-image hover-overlay ripple">
+                            <img class="card-img-top img-fluid rounded" style=" height: 30vh;" :src=" getPhoto() +'pexels-alex-azabache-3879160.jpg'" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Paris</div>
                                 <p class="card-text">
                                     Lorem ipsum in France
                                 </p>
-                            </div>
                         </div>
-                    </li>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <li class="list-group-item-action">
-                        <div class="card">
-                            <div class="bg-image hover-overlay ripple">
-                                <img class="card-img-top img-fluid rounded" :src="getPhoto() + 'pexels-chait-goli-1797161.jpg'" />
-                            </div>
-                            <div class="card-body">
-                                <div class="card-title">Rome</div>
+                <div class="col-md-5">
+                    <div class="card" style="width: 18em;">
+                        <div class="bg-image hover-overlay ripple">
+                            <img class="card-img-top img-fluid rounded" style=" height: 30vh;" :src=" getPhoto() +'pexels-chait-goli-1797161.jpg'" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Rome</div>
                                 <p class="card-text">
                                     Lorem ipsum in Rome
                                 </p>
-                            </div>
                         </div>
-                    </li>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <li class=" list-group-item-action">
-                        <div class="card">
-                            <div class="bg-image hover-overlay ripple">
-                                <img class="card-img-top img-fluid rounded" :src="getPhoto() + 'pexels-jimmy-teoh-951539.jpg'" />
-                            </div>
-                            <div class="card-body">
-                                <div class="card-title">Athens</div>
+                <div class="col-md-5">
+                    <div class="card" style="width: 18em;">
+                        <div class="bg-image hover-overlay ripple">
+                            <img class="card-img-top img-fluid rounded" style=" height: 30vh;" :src=" getPhoto() +'pexels-jimmy-teoh-951539.jpg'" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Athens</div>
                                 <p class="card-text">
                                     Lorem ipsum in Athens
                                 </p>
-                            </div>
                         </div>
-                    </li>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <li class="list-group-item-action">
-                        <div class="card">
-                            <div class="bg-image hover-overlay ripple">
-                                <img class="img-fluid" :src="getPhoto() + 'pexels-lina-kivaka-3336154.jpg'" />
-                            </div>
-                            <div class="card-body">
-                                <div class="card-title">London</div>
-                                <p class="card-text">
-                                    Lorem ipsum in London
-                                </p>
-                            </div>
+                <div class="col-md-5">
+                    <div class="card" style="width: 18em;">
+                        <div class="bg-image hover-overlay ripple">
+                            <img class="card-img-top img-fluid rounded" style=" height: 30vh;" :src=" getPhoto() +'pexels-lina-kivaka-3336154.jpg'" />
                         </div>
-                    </li>
+                        <div class="card-body">
+                            <div class="card-title">UK</div>
+                                <p class="card-text">
+                                    Lorem ipsum in UK
+                                </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </ul>
+        </div>
     </div>
 </template>
 
