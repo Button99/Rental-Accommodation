e96cf76e48c3d56daf2727cd8608fecc87154517
Button99 <christoskoubis@yahoo.com>
2021-12-29 01:58:34 +0200

    Search page style v1

--- RentalAccommodation/public/js/app.js
+++ RentalAccommodation/public/js/app.js
@@ -2551,6 +2551,90 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Search.vue?vue&type=script&lang=js&":
+/*!*************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Search.vue?vue&type=script&lang=js& ***!
+  \*************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  data: function data() {
+    return {
+      form: {
+        location: '',
+        checkIn: '',
+        checkOut: '',
+        guests: '',
+        rooms: ''
+      },
+      errors: []
+    };
+  }
+});
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -39060,15 +39144,17 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Search_vue_vue_type_template_id_5026ffd3___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Search.vue?vue&type=template&id=5026ffd3& */ "./resources/js/components/Search.vue?vue&type=template&id=5026ffd3&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Search.vue?vue&type=script&lang=js& */ "./resources/js/components/Search.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 ;
-var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Search_vue_vue_type_template_id_5026ffd3___WEBPACK_IMPORTED_MODULE_0__.render,
   _Search_vue_vue_type_template_id_5026ffd3___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
   false,
@@ -39194,6 +39280,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Register_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Register.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Register.vue?vue&type=script&lang=js&");
  /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Register_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/Search.vue?vue&type=script&lang=js&":
+/*!*********************************************************************!*\
+  !*** ./resources/js/components/Search.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Search.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Search.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Search_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
@@ -40757,15 +40859,152 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c("div", { staticClass: "search-layout" }, [
+    _c("div", { staticClass: "container-fluid w-25 p-3 h-75" }, [
+      _c(
+        "div",
+        { staticClass: "row d-flex justify-content-center align-items-center" },
+        [
+          _c("div", { staticClass: "card m-4" }, [
+            _c("h3", { staticClass: "card-header d-flex color-light h-75" }, [
+              _vm._v("Search"),
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "card-body text-left h-75 m-5" }, [
+              _c(
+                "form",
+                {
+                  attrs: { action: "#" },
+                  on: {
+                    submit: function ($event) {
+                      $event.preventDefault()
+                      return _vm.search()
+                    },
+                  },
+                },
+                [
+                  _vm._m(0),
+                  _vm._v(" "),
+                  _vm._m(1),
+                  _vm._v(" "),
+                  _vm._m(2),
+                  _vm._v(" "),
+                  _vm._m(3),
+                  _vm._v(" "),
+                  _vm._m(4),
+                  _vm._v(" "),
+                  _c(
+                    "button",
+                    {
+                      staticClass:
+                        "submit btn btn-primary text-uppercase fw-bold",
+                      attrs: { type: "submit" },
+                    },
+                    [_vm._v("Search ")]
+                  ),
+                ]
+              ),
+            ]),
+          ]),
+        ]
+      ),
+    ]),
+  ])
 }
 var staticRenderFns = [
   function () {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
-      _c("p", [_vm._v("This is the search page")]),
+    return _c("div", { staticClass: "form-outline mb-3" }, [
+      _c("label", { staticClass: "form-label", attrs: { for: "Location" } }, [
+        _vm._v(
+          " \n                                Location:\n                            "
+        ),
+      ]),
+      _vm._v(" "),
+      _c("br"),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "form-control",
+        attrs: { type: "text", name: "Location", id: "location" },
+      }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "form-outline mb-3" }, [
+      _c("label", { staticClass: "form-label", attrs: { for: "checkIn" } }, [
+        _vm._v(
+          " \n                                Check in:\n                            "
+        ),
+      ]),
+      _vm._v(" "),
+      _c("br"),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "form-control",
+        attrs: { type: "text", name: "checkIn", id: "checkIn" },
+      }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "form-outline mb-3" }, [
+      _c("label", { staticClass: "form-label", attrs: { for: "checkOut" } }, [
+        _vm._v(
+          " \n                                Check out:\n                            "
+        ),
+      ]),
+      _vm._v(" "),
+      _c("br"),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "form-control",
+        attrs: { type: "text", name: "checkOut", id: "checkOut" },
+      }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "form-outline mb-3" }, [
+      _c("label", { staticClass: "form-label", attrs: { for: "guests" } }, [
+        _vm._v(
+          " \n                                Guests:\n                            "
+        ),
+      ]),
+      _vm._v(" "),
+      _c("br"),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "form-control",
+        attrs: { type: "text", name: "guests", id: "guests" },
+      }),
+    ])
+  },
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "form-outline mb-3" }, [
+      _c("label", { staticClass: "form-label", attrs: { for: "rooms" } }, [
+        _vm._v(
+          " \n                                Rooms:\n                            "
+        ),
+      ]),
+      _vm._v(" "),
+      _c("br"),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "form-control",
+        attrs: { type: "text", name: "rooms", id: "rooms" },
+      }),
     ])
   },
 ]
--- RentalAccommodation/resources/js/components/Search.vue
+++ RentalAccommodation/resources/js/components/Search.vue
@@ -1,5 +1,72 @@
 <template>
-    <div class="container">
-        <p>This is the search page</p>
+    <div class="search-layout">
+        <div class="container-fluid w-25 p-3 h-75">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="card m-4">
+                    <h3 class="card-header d-flex color-light h-75">Search</h3>
+                    <div class="card-body text-left h-75 m-5">
+                        <form action="#" @submit.prevent="search()">
+                            <div class="form-outline mb-3">
+                                <label for="Location" class="form-label"> 
+                                    Location:
+                                </label>
+                                <br />
+                            <!-- Should find a way to store the locations -->
+                                <input type="text" name="Location" id="location" class="form-control" />
+                            </div>
+                            <div class="form-outline mb-3">
+                                <label for="checkIn" class="form-label"> 
+                                    Check in:
+                                </label>
+                                <br />
+                            
+                                <input type="text" name="checkIn" id="checkIn" class="form-control" />
+                            </div>
+                            <div class="form-outline mb-3">
+                                <label for="checkOut" class="form-label"> 
+                                    Check out:
+                                </label>
+                                <br />
+                                <input type="text" name="checkOut" id="checkOut" class="form-control" />
+                            </div>
+                            <div class="form-outline mb-3">
+                                <label for="guests" class="form-label"> 
+                                    Guests:
+                                </label>
+                                <br />
+                                <input type="text" name="guests" id="guests" class="form-control" />
+                            </div>
+                            <div class="form-outline mb-3">
+                                <label for="rooms" class="form-label"> 
+                                    Rooms:
+                                </label>
+                                <br />
+                                <input type="text" name="rooms" id="rooms" class="form-control" />
+                            </div>
+                            <button type="submit" class="submit btn btn-primary text-uppercase fw-bold">Search </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: {
+                    location: '',
+                    checkIn: '',
+                    checkOut: '',
+                    guests: '',
+                    rooms: ''
+                },
+                errors: []
+            }
+        }
+    }
+</script>
