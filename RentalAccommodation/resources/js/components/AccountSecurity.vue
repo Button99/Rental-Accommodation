98e28df492d08abca40159f1702dbbccf92241b0
Button99 <christoskoubis@yahoo.com>
2022-02-07 22:47:10 +0200

    CSS added to Account settings

--- RentalAccommodation/resources/js/components/AccountSecurity.vue
+++ RentalAccommodation/resources/js/components/AccountSecurity.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="security-and-login-layout">
-        <p>This is the Security and Login tab </p>
         <section class="settings-menu">
             <b-list-group >
                 <h2>Settings</h2>
@@ -10,25 +9,57 @@
                 <b-list-group-item to="/myAccount/deleteAccount"> Delete Account </b-list-group-item>
             </b-list-group>
         </section>
-        <!-- Need to implement functions with js -->
         <section class="settings-options">
             <div class="container mx-auto mt-4">
-                <h1>General Account Settings</h1>
+                <h1>Security and Login</h1>
                 <br />
                 <b-list-group>
                     <b-list-group-item><b-button v-b-toggle.collapse-changePassword block class="d-flex"><strong>Change password</strong>
-                        <strong v-if="isVisible" aria-hidden="true" class="ml-auto">-</strong>
+                        <strong v-if="isVisible == true" aria-hidden="true" class="ml-auto">-</strong>
                         <strong v-else aria-hidden="true" class="ml-auto">+</strong>
                         </b-button>
-                        <b-collapse id="collapse-changePassword" v-model="isVisible">
+                        <b-collapse id="collapse-changePassword" @hide="isVisible = false" @show="isVisible = true" v-model="visible">
                             <b-card title="Change Password">
-                                content
+                                <b-form @submit.prevent="changePassword()" @reset="onReset()" action="#" method="POST">
+                                    <b-form-group label="Old password: " label-for="old-password" class="mb-4">
+                                        <b-form-input id="old-password" v-model="form.oldPassword" type="password" required> </b-form-input>
+                                    </b-form-group>
+                                    <b-form-group label="New password: " label-for="new-password" class="mb-4">
+                                        <b-form-input id="new-password" v-model="form.newPassword" type="password" required> </b-form-input>
+                                    </b-form-group>
+                                    <b-form-group label-for="new-password-retype" label="New password (Retype): ">
+                                        <b-form-input id="new-password-retype" v-model="form.newPasswordRetype" type="password" required> </b-form-input>
+                                    </b-form-group>
+                                    <b-button type="submit">Change Password</b-button>
+                                </b-form>
                             </b-card>
                         </b-collapse>
                     </b-list-group-item>
-                    <b-list-group-item> Last Log in: </b-list-group-item>
+                    <b-list-group-item> Last Log in: {{data.ip}}</b-list-group-item>
                 <br />
+                </b-list-group>
             </div>
         </section>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isVisible: false,
+
+                form: {
+                    oldPassword: '',
+                    newPassword: '',
+                    newPasswordRetype: ''
+                },
+                
+                data: {
+                    ip: ''
+                }
+            }
+        }
+    }
+</script>
+
--- RentalAccommodation/resources/js/components/DeleteAccount.vue
+++ RentalAccommodation/resources/js/components/DeleteAccount.vue
@@ -1,16 +1,27 @@
 <template>
-<div class="container">
-    <br />
-    <h2>Delete account page </h2>
+<div class="delete-account-layout">
+    <section class="settings-menu">
+        <b-list-group >
+            <h2>Settings</h2>
+            <br />
+            <b-list-group-item to="/myAccount/general"> General </b-list-group-item>
+            <b-list-group-item to="/myAccount/security"> Security and Login </b-list-group-item>
+            <b-list-group-item to="/myAccount/deleteAccount"> Delete Account </b-list-group-item>
+        </b-list-group>
+    </section>
+
     <!-- To implement this page I need to make a validation:<template>
         User needs to type their first_name so they can confirm the action -->
     <section class="delete-account">
-        <p> To delete the account you need to write your First name</p>
-        <b-form @submit.prevent="deleteAccount()"  @reset="onReset()" action="#" method="POST">
-            <b-form-input type="text" v-model="form.first_name" placeholder="Enter your first name"></b-form-input>
-            <br />
-            <b-button type="submit" variant="danger">Delete Account </b-button>
-        </b-form>
+        <br />
+        <h3> To delete the account you need to write your First name</h3>
+        <div class="container mx-auto mt-4">
+            <b-form @submit.prevent="deleteAccount()"  @reset="onReset()" action="#" method="POST">
+                <b-form-input type="text" v-model="form.first_name" placeholder="Enter your first name"></b-form-input>
+                <br />
+                <b-button type="submit" variant="danger">Delete Account </b-button>
+            </b-form>
+        </div>
     </section>
 </div>
 </template>
