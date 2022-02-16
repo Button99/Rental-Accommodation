673fa055f2ff0d5967190aa80313cca0f10b02f5
Button99 <56029580+Button99@users.noreply.github.com>
2022-02-16 19:57:18 +0200

    Create Accommodation page v1

--- RentalAccommodation/resources/js/components/CreateAccommodation.vue
+++ RentalAccommodation/resources/js/components/CreateAccommodation.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="create-accommodation-layout">
-        <div class="container-fluid w-40 p-4 h-90">
-            <div class="row d-flex justify-content-center align-items-center">
-                <b-card header="Create Accommodation">
+        <div class="container-fluid w-100 p-4 h-90">
+            <div class="row d-flex justify-content-left align-items-left">
+                <b-card header="Create Accommodation" class="w-40" >
                     <b-card-body class="text-center">
                         <b-form-group action="#" @submit.prevent="createAccommodation()" method="POST">
                             <b-form-group label="Name: " label-for="name">
@@ -24,7 +24,32 @@
                     </b-card-body>
                 </b-card>
             </div>
+            <div>
+                <b-form-group label="Features">
+                    <b-form-row name="group1">
+                        <b-col><b-form-checkbox value="1" v-model="form.pool" name="pool">Pool</b-form-checkbox></b-col>
+                        <b-col><b-form-checkbox value="1" v-model="form.bbq" name="bbq">BBQ</b-form-checkbox></b-col>
+                        <b-col><b-form-checkbox value="1" v-model="form.pool_table" name="pool_table">Pool table</b-form-checkbox></b-col>
+                    </b-form-row>
+                    <b-form-checkbox-group name="group2">
+                        <b-form-checkbox value="1" v-model="form.wifi" name="wifi">Wifi</b-form-checkbox>
+                        <b-form-checkbox value="1" v-model="form.tv" name="tv">TV</b-form-checkbox>
+                        <b-form-checkbox value="1" v-model="form.kitchen" name="kitchen">Kitchen</b-form-checkbox>
+                    </b-form-checkbox-group>
+                    <b-form-checkbox-group name="group3">
+                        <b-form-checkbox value="1" v-model="form.parking" name="parking">Parking</b-form-checkbox>
+                        <b-form-checkbox value="1" v-model="form.air_conditioning" name="air_conditioning">Air conditioning</b-form-checkbox>
+                        <b-form-checkbox value="1" v-model="form.washer" name="washer">Washer</b-form-checkbox>
+                    </b-form-checkbox-group>
+                    <b-form-checkbox-group name="group4">
+                        <b-form-checkbox value="1" v-model="form.fire_extinguisher" name="fire_extinguisher">Fire extinguisher</b-form-checkbox>
+                        <b-form-checkbox value="2" v-model="form.smoke_alarm" name="smoke_alarm">Smoke alarm</b-form-checkbox>
+                        <b-form-checkbox value="1" v-model="form.hot_tub" name="hot_tub">Hot tub</b-form-checkbox>
+                    </b-form-checkbox-group>
+                </b-form-group>
+            </div>
         </div>
+
     </div>
 </template>
 
