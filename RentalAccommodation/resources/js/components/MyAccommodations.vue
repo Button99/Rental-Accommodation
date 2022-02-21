2d50bbd996485380f2acfcbf77bb7bc3940040ca
Button99 <56029580+Button99@users.noreply.github.com>
2022-02-21 18:31:54 +0200

    MyAccommodations css v2

--- RentalAccommodation/resources/js/components/MyAccommodations.vue
+++ RentalAccommodation/resources/js/components/MyAccommodations.vue
@@ -3,49 +3,19 @@
         <h1>My Accommodations Page</h1>
         <!-- here should i implement a if -->
         <section class="my-accommodations">
-            <ul>
-                <div class="col-md-5">
+            <h1> {{test}} </h1>
+            <ul class="justify-content-center">
+                <div class="col-md-7">
                 <!-- here i should implement a for !-->
-                    <li>
+                    <li v-for="accommodation in accommodations" class="p-3 col-md-4 mt-5">
                         <div class="card">
                             <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
                             <div class="card-body">
-                                <h5 class="card-title">Title</h5>
+                                <h5 class="card-title">{{accommodation.name}}</h5>
                                 <p class="card-text">
-                                    Accommodation data
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                                        <li>
-                        <div class="card">
-                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
-                            <div class="card-body">
-                                <h5 class="card-title">Title</h5>
-                                <p class="card-text">
-                                    Accommodation data
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                                        <li>
-                        <div class="card">
-                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
-                            <div class="card-body">
-                                <h5 class="card-title">Title</h5>
-                                <p class="card-text">
-                                    Accommodation data
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                                        <li>
-                        <div class="card">
-                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
-                            <div class="card-body">
-                                <h5 class="card-title">Title</h5>
-                                <p class="card-text">
-                                    Accommodation data
+                                    Rooms: {{accommodation.rooms}} <br />
+                                    Town: {{accommodation.town}} <br />
+                                    Description: {{accommodation.description}} <br />
                                 </p>
                             </div>
                         </div>
@@ -55,3 +25,41 @@
         </section>
     </div>
 </template>
+
+<script>
+import AppStorage from '../helpers/AppStorage';
+
+    export default {
+        data() {
+            return {
+                accommodations: []
+            }
+        },
+    
+    created() {
+        this.fetchData();
+    },
+
+    methods: {
+        fetchData() {
+            axios.get('api/user', {
+                headers: {
+                    Authorization: 'Bearer ' + JSON.parse(AppStorage.getToken())
+                }
+                }).then((res) => {
+                    axios.get('api/'+ res.data.id + '/myAccommodations', {
+                        headers: {
+                            Authorization: 'Bearer ' + JSON.parse(AppStorage.getToken())
+                        }
+                    }).then((res) => {
+                        this.accommodations= res.data;
+                    }).catch((err) => {
+                        alert(err);
+                    })
+                }).catch((err) => {
+                    alert(err);
+                });
+            }
+        }
+    }
+</script>
