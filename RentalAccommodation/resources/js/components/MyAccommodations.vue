427987f4a3f768d89007b7a833cd38a9e305533a
Button99 <56029580+Button99@users.noreply.github.com>
2022-02-21 16:49:56 +0200

    MyAccommodations css added  v1

--- RentalAccommodation/resources/js/components/MyAccommodations.vue
+++ RentalAccommodation/resources/js/components/MyAccommodations.vue
@@ -1,5 +1,57 @@
 <template>
     <div class="container">
         <h1>My Accommodations Page</h1>
+        <!-- here should i implement a if -->
+        <section class="my-accommodations">
+            <ul>
+                <div class="col-md-5">
+                <!-- here i should implement a for !-->
+                    <li>
+                        <div class="card">
+                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
+                            <div class="card-body">
+                                <h5 class="card-title">Title</h5>
+                                <p class="card-text">
+                                    Accommodation data
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                                        <li>
+                        <div class="card">
+                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
+                            <div class="card-body">
+                                <h5 class="card-title">Title</h5>
+                                <p class="card-text">
+                                    Accommodation data
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                                        <li>
+                        <div class="card">
+                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
+                            <div class="card-body">
+                                <h5 class="card-title">Title</h5>
+                                <p class="card-text">
+                                    Accommodation data
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                                        <li>
+                        <div class="card">
+                            <img src="pictures/pexels-alex-azabache-3879160.jpg" style=" height: 30vh;" class="card-img-top" />
+                            <div class="card-body">
+                                <h5 class="card-title">Title</h5>
+                                <p class="card-text">
+                                    Accommodation data
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+            </ul>
+        </section>
     </div>
 </template>
