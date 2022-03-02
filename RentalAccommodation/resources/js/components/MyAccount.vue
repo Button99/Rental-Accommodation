481b3183f5d71054c37c6ec540edc342ae6358c8
Button99 <56029580+Button99@users.noreply.github.com>
2022-03-02 16:04:29 +0200

    Sync data

--- RentalAccommodation/resources/js/components/MyAccount.vue
+++ RentalAccommodation/resources/js/components/MyAccount.vue
@@ -1,30 +1,29 @@
-<template>
-<div class="my-account-layout">
-    <section class="settings-menu">
-        <b-list-group >
-            <h2>Settings</h2>
-            <br />
-            <b-list-group-item to="/myAccount/general"> General </b-list-group-item>
-            <b-list-group-item to="/myAccount/security"> Security and Login </b-list-group-item>
-            <b-list-group-item to="/myAccount/deleteAccount"> Delete Account </b-list-group-item>
-        </b-list-group>
-    </section>
-    <section class="settings-options">
-        <div class="container mx-auto mt-4">
-            <h1>General Account Settings</h1>
-            <br />
-            <b-list-group>
-                <b-list-group-item> Name: <b>{{first_name}}  {{last_name}}</b></b-list-group-item>
-                <b-list-group-item> Phone: <b>{{phone}}</b></b-list-group-item>
-                <b-list-group-item> Email: <b>{{email}}</b></b-list-group-item> <!-- Needs to go to security tab -->
-                <b-list-group-item> Gender: <b>{{gender}}</b></b-list-group-item>
+<template v-if="load === '1'">
+    <div class="my-account-layout">
+        <section class="settings-menu">
+            <b-list-group >
+                <h2>Settings</h2>
+                <br />
+                <b-list-group-item to="/myAccount/general"> General </b-list-group-item>
+                <b-list-group-item to="/myAccount/security"> Security and Login </b-list-group-item>
+                <b-list-group-item to="/myAccount/deleteAccount"> Delete Account </b-list-group-item>
             </b-list-group>
-            <br />
-        </div>
-    </section>
-</div>
+        </section>
+        <section class="settings-options">
+            <div class="container mx-auto mt-4">
+                <h1>General Account Settings</h1>
+                <br />
+                <b-list-group>
+                    <b-list-group-item> Name: <b>{{first_name}}  {{last_name}}</b></b-list-group-item>
+                    <b-list-group-item> Phone: <b>{{phone}}</b></b-list-group-item>
+                    <b-list-group-item> Email: <b>{{email}}</b></b-list-group-item> <!-- Needs to go to security tab -->
+                    <b-list-group-item> Gender: <b>{{gender}}</b></b-list-group-item>
+                </b-list-group>
+                <br />
+            </div>
+        </section>
+    </div>
 </template>
-
 <script>
     import AppStorage from '../helpers/AppStorage';
 
@@ -36,6 +35,7 @@
                 phone: '',
                 email: '',
                 gender: '',
+                load: '0'
             }
         },
 
@@ -54,6 +54,7 @@
                     this.phone=res.data.phone;
                     this.email= res.data.email;
                     this.gender= res.data.gender;
+                    this.load= '1';
                 });
             }
         }
