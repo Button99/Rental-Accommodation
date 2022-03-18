e9b2309aabfc89f7cc9e523a027d2b197ad6a401
Button99 <56029580+Button99@users.noreply.github.com>
2022-03-18 23:19:24 +0200

    Show all Accommodations notes & code

--- RentalAccommodation/resources/js/components/Accommodations.vue
+++ RentalAccommodation/resources/js/components/Accommodations.vue
@@ -1,5 +1,38 @@
 <template>
     <div class="accommodations-layout">
         <h3>Here you can find the best accommodations</h3>
+        
+        <!-- I should implement with v-for  (just like myAccommodations) -->
+        <section>
+            <!-- Create a card just like the myAccommodations -->
+            <!-- Also, create a menu where user can choose -->
+            <!-- ex. cost, number of rooms etc.. -->
+        </section>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                accommodations: []
+            }
+        },
+
+        created() {
+            this.fetchData();
+        },
+
+        methods: {
+            fetchData() {
+                axios.get('api/accommodations')
+                    .then((res) => {
+                        this.accommodations= res.data;
+                        console.log(this.accommodations);
+                    }).catch((err) => {
+                        alert(err);
+                    })
+            }
+        }
+    }
+</script>
