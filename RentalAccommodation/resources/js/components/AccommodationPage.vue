0b2f920d4f89c53249ff24f71c46deda10453e37
Button99 <56029580+Button99@users.noreply.github.com>
2022-05-14 11:41:16 +0300

    Open Street map API

--- RentalAccommodation/resources/js/components/AccommodationPage.vue
+++ RentalAccommodation/resources/js/components/AccommodationPage.vue
@@ -12,6 +12,7 @@
                         Type: {{accommodation.accommodation_type}} <br />
                         Rooms: {{accommodation.rooms}} <br />
                         Town: {{accommodation.town}} <br />
+                        Address: {{accommodation.address1}}, {{accommodation.address2}} <br />
                         Description: {{accommodation.description}} <br />
                         <b v-if="accommodation.stars == null ">
                             Rate: Not available
@@ -19,7 +20,7 @@
                         <b v-else>
                             Rate: {{accommodation.stars}}
                         </b>
-                    </p>
+                    </p>                  
                     <b-list-group v-if=" accommodation.user_id == usr_id" horizontal="md">
                         <b-list-group-item>
                             <router-link :to="/updateAccommodation/ + accommodation.id"> <b-button variant="warning" type="submit"> Update </b-button> </router-link>
@@ -35,16 +36,39 @@
             </div>
         </div>
         <br />
+        <div style="">
+            <l-map style="height: 80vh; width: 50%" :zoom="zoom" :center="center">
+                <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+                <l-marker :lat-lng="markerLatLng"></l-marker>
+            </l-map>  
+        </div>
     </section>
 </template>
 
 <script>
 import Accommodation from '../helpers/Accommodation';
 import AppStorage from '../helpers/AppStorage';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+ 
 
 export default {
+        components: {
+            LMap,
+            LTileLayer,
+            LMarker
+        },
+        
         data() {
             return {
+                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution:
+                    '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                center: [51.505, -0.159],
+                zoom: 15,
+                markerLatLng: [51.504, -0.159],
                 accommodation: [],
                 usr_id: '',
                 pictures: 'null',
@@ -60,6 +84,7 @@ export default {
                 axios.get('api/accommodations/'+ this.$route.params.id)
                     .then((res) => {
                         this.accommodation= res.data.accommodation;
+                        console.log(res.data.accommodation);
                         this.pictures= res.data.pictures;
                         console.log(this.pictures[0]);
                     }).catch((err) => {
