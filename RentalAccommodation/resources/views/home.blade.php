9994a78a42950c49e44f93f17d0f11929d5e8b45
Button99 <56029580+Button99@users.noreply.github.com>
2022-06-15 21:43:53 +0300

    Google Maps API Removed

--- RentalAccommodation/resources/views/home.blade.php
+++ RentalAccommodation/resources/views/home.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Rental Accommodation</title>
-        <script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAwkX_DWKXLkv1LTOZdfnGkvfre8BY3zww"></script>
 
         <link rel="stylesheet" src="./sass/app.scss" />
     </head>
