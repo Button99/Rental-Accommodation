b44a52744ba20e91968d47729b973f1d0375aec0
Button99 <56029580+Button99@users.noreply.github.com>
2022-06-28 22:02:37 +0300

    Footer Bugs v1

--- RentalAccommodation/resources/js/components/Footer.vue
+++ RentalAccommodation/resources/js/components/Footer.vue
@@ -1,16 +1,6 @@
 <template>
     <div>
         <footer class="footer justify-content-center d-flex" id="footer">
-            <b-card header="Information" id="footer-card" style="background-color: #E4748C;">
-                <b-card-body>
-                    <p>Lorem ipsum</p>
-                </b-card-body>
-            </b-card>
-            <b-card header="Support" id="footer-card" style="background-color: #DB7798;">
-                <b-card-body class="card-body">
-                    <p>Lorem ispum</p>
-                </b-card-body>
-            </b-card>
             <p>&copy; RentalAccommodation</p>
         </footer>
     </div>
--- RentalAccommodation/resources/js/components/Index.vue
+++ RentalAccommodation/resources/js/components/Index.vue
@@ -68,11 +68,11 @@
             <div class="row text-center">
                 <p class="col-md-5 border-right border-bottom" id="how-to-book"> How to book
                     <br />
-                    TO BOOK YOUR TRAVEL ALL YOU NEED TO DO IS TO LOGIN AND THEN YOU MUST
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <p class="col-md-5 border-bottom" id="about-us"> About us
                     <br />
-                    THIS WEBSITE HAS BEEN MADE FROM ...
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
         </div>
