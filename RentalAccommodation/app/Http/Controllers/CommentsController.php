fe1157845ed0883492fb90706d1271b6020d350d
Button99 <56029580+Button99@users.noreply.github.com>
2022-07-27 20:44:02 +0300

    Show all comments

--- RentalAccommodation/app/Http/Controllers/CommentsController.php
+++ RentalAccommodation/app/Http/Controllers/CommentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comments;
+use Egulias\EmailValidator\Warning\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Response;
@@ -29,6 +30,12 @@ class CommentsController extends Controller
         }
         return response()->json('Comment can\'t be created!', Response::HTTP_BAD_REQUEST);
 
+    }
+
+    public function index() {
+        $comments= Comments::orderBy('updated_at', 'desc')->get();
+
+        return response()->json($comments, Response::HTTP_ACCEPTED);
     }
 
     public function update(Request $request, Comments $comments)
