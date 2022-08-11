eacbe5ad5337817cd01a5c7da4f557ada7794cac
Button99 <56029580+Button99@users.noreply.github.com>
2022-08-11 15:34:40 +0300

    Added Seeder

--- RentalAccommodation/database/seeders/DatabaseSeeder.php
+++ RentalAccommodation/database/seeders/DatabaseSeeder.php
@@ -6,8 +6,9 @@ use App\Models\Accommodation;
 use App\Models\Picture;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use App\Models\Booking;
+use App\Models\Comment;
 use App\Models\Feature;
+use App\Models\Rate;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,10 +19,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // με την χρήση του 10 εννοούμε ότι θέλουμε να φτίαξουμε 10 εγγραφές.
         User::factory(10)->create();
         Accommodation::factory(10)->create();
-        Picture::factory(10)->create();
-        Booking::factory(10)->create();
+        Comment::factory(10)->create();
         Feature::factory(10)->create();
+        Picture::factory(10)->create();
+        Rate::factory(10)->create();
+        Picture::factory(10)->create();        
     }
 }
