ee3a8eabdfc8e1ef41b0b1f84a1e654721135a30
Button99 <56029580+Button99@users.noreply.github.com>
2022-08-18 13:08:07 +0300

    Verify Email CSS

--- RentalAccommodation/resources/js/components/VerifyEmail.vue
+++ RentalAccommodation/resources/js/components/VerifyEmail.vue
@@ -1,13 +1,14 @@
 <template>
-<p>Your email has been verified!</p>
+    <div class="verify-layout">
+        <h2>Your email has been verified!</h2>
+        <p>You can now Log in</p>
+    </div>
 </template>
 
 <script>
 export default {
 
     created() {
-        // , this.$route.query.vkey
-        console.log(this.$route.query.vkey)
         User.verifyUser(this.$route.query.vkey);
     },
 }
