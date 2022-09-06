b5c1bb535e34fc9c14e94eb77d451a53ce44d4ce
Button99 <56029580+Button99@users.noreply.github.com>
2022-09-06 10:46:48 +0300

    Responsive ForgotPassword

--- RentalAccommodation/resources/js/components/ForgotPassword.vue
+++ RentalAccommodation/resources/js/components/ForgotPassword.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="login-layout">
-        <div class="container p-5">
+        <div class="container">
             <div class="row d-flex justify-content-center align-items-center m-5">
                 <b-card class="shadow-3-strong m-4 color-light border-0 d-flex" header-class="h3" header="Forgot Password" >
-                    <b-card-body class="text-center m-5">
+                    <b-card-body class="text-center fixed-height">
                         <div v-if="!this.$route.query.key">
                             <b-form @submit.prevent="forgotPassword()" @reset="onReset()" action="#" method="POST">
                                 <b-form-group label="Email: " label-for="email" class="mb-4 ">
