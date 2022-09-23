ff3f7f0ac8aa7f85514f50d632aadd94cad5f985
Button99 <56029580+Button99@users.noreply.github.com>
2022-09-23 09:32:09 +0300

    Clear commit

--- RentalAccommodation/resources/js/components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <footer class="footer justify-content-center d-flex" id="footer">
-            <p>&copy; RentalAccommodation</p>
-        </footer>
-    </div>
-</template>
